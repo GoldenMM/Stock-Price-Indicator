f5b90f5a25216826eaaeeba31aa6ca4768a09f96
GoldenMM <ryanjanderton@yahoo.com.au>
2024-08-14 12:42:57 +0800

    renaming for convention

--- buildDataset.py
+++ buildDataset.py
@@ -1,6 +1,6 @@
 '''
 Purpose of this script is to build the initial dataset of stock data.
-This is not to be confused with the update_dataset.py script which is used to update the dataset
+This is not to be confused with the updateDataset.py script which is used to update the dataset
 based on mising days
 '''
 
