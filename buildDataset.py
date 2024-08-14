bfc0f959615a2166ddaf92a2ae6e1a531d8a6b92
GoldenMM <ryanjanderton@yahoo.com.au>
2024-08-14 15:03:19 +0800

    historic dataset collected

--- buildDataset.py
+++ buildDataset.py
@@ -6,4 +6,30 @@ based on mising days
 
 import yfinance as yf
 import pandas as pd
+from tqdm import tqdm
 
+def getHistoricFromTicker(ticker: str) -> pd.DataFrame:
+    '''
+    Returns cleaned data from yfinance request. We will be using a period of 5 years to
+    keep the dataset manageable.
+    '''
+    df = yf.Ticker(ticker).history(period='5y') # Get the data
+    df2 = df[['Open', 'High', 'Low', 'Close', 'Volume']].copy() #only get the rows we want
+    if (df2.shape[0] == 0):
+        raise ValueError(f"No data found for ticker {ticker} in the specified range")
+    return df2
+
+
+# Pull in the stock tickers
+tickers = pd.read_csv('data/stock_tickers.csv')
+print(tickers.head())
+
+# Iterate over each ticker and save the historical data to CSV files
+for ticker in tqdm(tickers['Ticker'], desc='Processing tickers'):
+    try:
+        df = getHistoricFromTicker(ticker)
+        df.to_csv(f'data/stock_data/{ticker}.csv', index=True)
+        print(f"Saved data for {ticker}")
+    except Exception as e:
+        print(f"Failed to save data for {ticker}: {e}")
+        continue
--- normalizeTickers.py
+++ normalizeTickers.py
@@ -1,6 +1,8 @@
-'''Takes the stock_tickers.txt file and extracts the ticker code and company name.
-   Most likely to be redundant now it has run once, but may need to be used if the
-   NASDAQ listings ever change.'''
+'''
+Takes the stock_tickers.txt file and extracts the ticker code and company name.
+Most likely to be redundant now it has run once, but may need to be used if the
+NASDAQ listings ever change.
+'''
 
 import pandas as pd
 
@@ -16,7 +18,7 @@ def normaliseCompanyName(name: str) -> str:
     return new_name
 
 # Get the needed data from the stock_tickers.txt file
-df = pd.read_csv('stock_tickers.txt', delimiter='|')
+df = pd.read_csv('data/stock_tickers.txt', delimiter='|')
 df2 = df[['Symbol', 'Security Name']].copy()
 df2.columns = ['Ticker', 'Company']
 df2.dropna()                        # Drop null rows
@@ -24,4 +26,4 @@ df2 = df2.convert_dtypes()          # Ensure datatypes are proper
 
 # Normalize the company names and save the data
 df2['Company'] = df2['Company'].map(normaliseCompanyName)
-df2.to_csv('stock_tickers.csv', index=False)
+df2.to_csv('data/stock_tickers.csv', index=False)
