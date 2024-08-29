8a40af43d72be1d24d8b1feddaef776d6960c0f2
GoldenMM <ryanjanderton@yahoo.com.au>
2024-08-30 07:53:19 +0800

    bulk commit

--- cleanDates.py
+++ cleanDates.py
@@ -7,7 +7,7 @@ import pandas as pd
 import os
 from datetime import date
 import datetime
-
+from tqdm import tqdm
 
 def cleanStockDate(ticker: str):
     '''
@@ -16,7 +16,19 @@ def cleanStockDate(ticker: str):
 
     # Read the csv
     data_path = f'data/stock_data/{ticker}.csv'
-    df = pd.read_csv(data_path, index_col=None, parse_dates=['Date'])
+    df = pd.read_csv(data_path, parse_dates=['Date'])
+    # Convert the Date col
+    df['Date'] = pd.to_datetime(df['Date'], utc=True).dt.date
+    # Save the csv
+    df = df[['Date','Open','High','Low','Close','Volume']]
+    df.to_csv(data_path, index='Date')
 
+# Get all the tickers
+tickers = pd.read_csv('data/stock_tickers.csv', index_col=None)
 
-cleanStockDate('')
+# Loop through all and clean the dates
+# for ticker in tqdm(tickers['Ticker'], desc='Cleaning dates'):
+#     print(f'Cleaning {ticker}:')
+#     cleanStockDate(ticker)
+
+cleanStockDate('AACG')
--- updateDataset.py
+++ updateDataset.py
@@ -5,30 +5,54 @@
 import yfinance as yf
 import pandas as pd
 import os
-from datetime import date
+from datetime import date, timedelta
 import datetime
+from tqdm import tqdm
 
 
 def updateStockTicker(ticker: str):
     '''
     This will update an individual stockdata.csv file from the ticker
     '''
-    # today = date.today()
-    today = datetime.datetime.fromisoformat('2024-08-13 00:00:00-04:00')
-    
+    today = date.today()
+
     # Get the current dataframe for the ticker
     data_path = f'data/stock_data/{ticker}.csv'
-    df = pd.read_csv(data_path, index_col=None, parse_dates=['Date'])
+    df = pd.read_csv(data_path, index_col=0, parse_dates=['Date'])
 
-    print(df.dtypes)
-    print(df.iloc[-1].iat[0])
-    print(df['Date'].dtype)
-    print(today)
+    df['Date'] = pd.to_datetime(df['Date'], utc=True).dt.date
 
-    if (today == df.iloc[-1].iat[0].date):
+    last_entry = df.iloc[-1].iat[0]
+    yesterday = today - timedelta(days=1)
+    
+    # Check if it needs updating. If not do nothing
+    if (yesterday == last_entry):
         print(f'{ticker}.csv is up to date.')
-    else:
-        print(f'{ticker}.csv is not up to date.')
+        return 
+    
+    print(f'{ticker}.csv is not up to date.')
+    # Get the tickets to date from last entry
+    recent = yf.Ticker(ticker).history(start=last_entry + timedelta(days=1), end = today)
+    recent_clean = recent[['Open', 'High', 'Low', 'Close', 'Volume']].copy()
+    recent_clean = recent_clean.reset_index()
+    recent_clean['Date'] =  pd.to_datetime(recent_clean['Date']).dt.date
+    
+    # Concat the two dataframes
+    updated_df = pd.concat([df, recent_clean])
+    updated_df = updated_df.reset_index(drop=True)
+    
+    # Save the .csv
+    updated_df.to_csv(data_path)
+ 
+# Run it for all stocks
+
+# Get all the tickers
+tickers = pd.read_csv('data/stock_tickers.csv', index_col=None)
+
+# Loop through all and clean the dates
+for ticker in tqdm(tickers['Ticker'], desc='Updating stock data...'):
+    print(f'Updating {ticker}:')
+    updateStockTicker(ticker)
 
 
 
